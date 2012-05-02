fb64c4852bef03c9d2d674e0f157affe347ea3f0
Matthijs Groen <matthijs.groen@gmail.com>
2012-05-02 10:20:26 +0200

    Executed rails template
    see: https://gist.github.com/2574804

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KKSBackbone::Application.config.secret_token = '605cea8f860e7a49efda8c6e06d956c84c027fe3d25544edfce8950dd33ec56b95d941f17abcc7314b6bcec45164034cddc248ed85ad9b914a28b1d5fc0a184c'
